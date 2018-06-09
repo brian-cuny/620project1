94c2a57043471e0c665f7d00379abff1ccadb76f
brian-w-projects <brian.weinfeld@gmail.com>
2018-06-09 12:05:59 -0400

    created subgraphs

--- python_to_neo4j.py
+++ python_to_neo4j.py
@@ -3,13 +3,12 @@ import csv
 from itertools import islice
 
 '''
-This program will import the roadNet-CA.txt file (containing 1965206 nodes and 5533214 edges)
-into a Neo4j graph and then query a small subset of that and add it to a network x graph.
-All nodes within 3 steps of road id 0 are stored in subgraph.csv for further processing
+This program will import open flights text files, clean them and then add them to a neo4j database for 
+further querying.
 '''
 
 
-class RoadNet(object):
+class OpenFlights(object):
 
     def __init__(self, uri, user, password):
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -23,26 +22,40 @@ class RoadNet(object):
 
 
 if __name__ == '__main__':
-    neo = RoadNet('bolt://localhost:7687', 'neo4j', 'password')
+    neo = OpenFlights('bolt://localhost:7687', 'neo4j', 'password')
 
     neo.query('MATCH (n) DETACH DELETE n')
-    neo.query('CREATE CONSTRAINT ON (r:Road) ASSERT r.id IS UNIQUE')
+    neo.query('CREATE CONSTRAINT ON (r:Airport) ASSERT r.id IS UNIQUE')
 
-    with open('roadNet-CA.txt', newline='') as csvfile:
-        next(islice(csvfile, 4, 4), None)
-        for r in csv.reader(csvfile, delimiter='\t'):
-            neo.query(f'MERGE (r1:Road {{id: {r[0]}}})'
-                      f'MERGE (r2:Road {{id: {r[1]}}})'
-                      'CREATE (r1)-[:CONNECTS]->(r2)')
+    airport_id = []
+    with open('openflights_airports.txt', newline='') as csvfile:
+        next(islice(csvfile, 1, 1), None)
+        for r in csv.reader(csvfile, delimiter=' '):
+            if r[3] in ['Canada', 'United Kingdom']:
+                r[1] = r[1].replace("'", "")
+                airport_id.append(r[0])
+                neo.query(f"MERGE (a1:Airport {{id: {r[0]}, name: '{r[1]}', country: '{r[3]}' }}) ")
 
-    results = [(r1, r2) for r1, r2 in neo.query('MATCH p=(:Road {id: 0})-[:CONNECTS*1..3]->(:Road) '
-                                                'WITH p '
-                                                'MATCH (r3:Road)-[:CONNECTS*1..3]->(r4:Road) '
-                                                'WHERE r3 IN nodes(p) AND r4 in nodes(p) AND r3 <> r4 '
-                                                'RETURN DISTINCT r3.id, r4.id')]
+    with open('openflights.txt', newline='') as csvfile:
+        for r in csv.reader(csvfile, delimiter=' '):
+            if r[0] in airport_id and r[1] in airport_id:
+                neo.query(f"MATCH (a1: Airport {{id: {r[0]}}}) "
+                          f"MATCH (a2: Airport {{id: {r[1]}}}) "
+                          f"CREATE (a1)-[c:CONNECTS]->(a2) "
+                          f"SET c.routes = {r[2]}")
 
-    print(results)
+    #508 airports with 1924 connections most resiprocal but not all
 
-    with open('subgraph.csv', 'w') as write_file:
+    results = neo.query('MATCH (a1:Airport) '
+                        'RETURN a1.id, a1.name, a1.country')
+
+    with open('airports_sub.csv', 'w') as write_file:
         writer = csv.writer(write_file)
         writer.writerows(results)
+
+    results2 = neo.query('MATCH (a1:Airport)-[c:CONNECTS]->(a2:Airport) '
+                         'RETURN a1.id, a2.id, c.routes')
+
+    with open('connections_sub.csv', 'w') as write_file:
+        writer = csv.writer(write_file)
+        writer.writerows(results2)
